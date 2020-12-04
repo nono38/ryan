eb8f1e65a83663d78d16d5a340b14bad33f3dbd5
nono38 <74985866+nono38@users.noreply.github.com>
2020-12-04 20:54:21 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,32 +1,53 @@
+input.onButtonPressed(Button.A, function () {
+    if (_16 == 0 && (_12 == 0 && _8 == 1)) {
+        SOMME_ILIAN = SOMME_ILIAN - 10
+    } else {
+        if (_16 == 0 && (_12 == 1 && _8 == 0)) {
+            SOMME_NOAH = SOMME_NOAH - 10
+        } else {
+            if (_16 == 1 && (_12 == 1 && _8 == 0)) {
+                SOMME_RYAN = SOMME_RYAN - 10
+            } else {
+                if (_16 == 0 && (_12 == 1 && _8 == 1)) {
+                    SOMME_PAPA = SOMME_PAPA - 10
+                }
+            }
+        }
+    }
+})
 input.onButtonPressed(Button.AB, function () {
     basic.clearScreen()
     _16 = pins.digitalReadPin(DigitalPin.P16)
-    basic.showString("" + (_16))
     basic.pause(100)
     basic.clearScreen()
     basic.pause(100)
     _12 = pins.digitalReadPin(DigitalPin.P12)
-    basic.showString("" + (_12))
     basic.pause(100)
     basic.clearScreen()
     basic.pause(100)
     _8 = pins.digitalReadPin(DigitalPin.P8)
-    basic.showString("" + (_8))
     basic.pause(100)
     basic.clearScreen()
     basic.pause(100)
     if (_16 == 0 && (_12 == 0 && _8 == 1)) {
         basic.showString("carte Ilian ")
+        basic.pause(100)
         basic.showString("" + (SOMME_ILIAN))
     } else {
         if (_16 == 0 && (_12 == 1 && _8 == 0)) {
             basic.showString("carte Noah")
+            basic.pause(100)
+            basic.showString("" + (SOMME_NOAH))
         } else {
             if (_16 == 1 && (_12 == 1 && _8 == 0)) {
                 basic.showString("carte Ryan")
+                basic.pause(100)
+                basic.showString("" + (SOMME_RYAN))
             } else {
                 if (_16 == 0 && (_12 == 1 && _8 == 1)) {
                     basic.showString("carte Papa")
+                    basic.pause(100)
+                    basic.showString("" + (SOMME_PAPA))
                 }
             }
         }
@@ -43,12 +64,18 @@ input.onButtonPressed(Button.B, function () {
         if (_16 == 0 && (_12 == 1 && _8 == 0)) {
             SOMME_NOAH = SOMME_NOAH + 10
         } else {
-            if (true) {
-            	
+            if (_16 == 1 && (_12 == 1 && _8 == 0)) {
+                SOMME_RYAN = SOMME_RYAN + 10
+            } else {
+                if (_16 == 0 && (_12 == 1 && _8 == 1)) {
+                    SOMME_PAPA = SOMME_PAPA + 10
+                }
             }
         }
     }
 })
+let SOMME_PAPA = 0
+let SOMME_RYAN = 0
 let SOMME_NOAH = 0
 let _8 = 0
 let _12 = 0
@@ -58,3 +85,8 @@ pins.setPull(DigitalPin.P8, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P12, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
 SOMME_ILIAN = 0
+basic.forever(function () {
+    _16 = pins.digitalReadPin(DigitalPin.P16)
+    _12 = pins.digitalReadPin(DigitalPin.P12)
+    _8 = pins.digitalReadPin(DigitalPin.P8)
+})
